cd2bd011d42df68de0bc7db096890044c2de4892
ooooo5h <ooooo5h91@gmail.com>
2021-12-23 16:20:20 +0900

    제작한 함수 사용 예시

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ def add_two_numbers(num1, num2):
     result = num1 + num2
     return result
     
+a = add_two_numbers(5, 2)  # 함수에 return이 있다면, 그 결과를 변수에 담거나 / print 등으로 일반변수처럼 활용 가능
+print(a)
+
+b = add_two_numbers(10, 4)
+print(b)
+
+print(f'11과 15의 합은 {add_two_numbers(11, 15)}입니다.')
